b7ad9cd4f4f7ad8959be26878a42f4e8718bf093
sanjeev-rm <sanjeevraghu2050@gmail.com>
2023-04-08 12:47:18 +0530

    Made the selecting date functionality work. Changed the UI.

--- Spaceopedia/APOD/ApodTableViewController.swift
+++ Spaceopedia/APOD/ApodTableViewController.swift
@@ -32,7 +32,7 @@ class ApodTableViewController: UITableViewController {
         
         datePicker.maximumDate = Date()
         
-        initialViewUpdate()
+        fetchingApodViewUpdate()
         
         Task {
             do {
@@ -45,16 +45,12 @@ class ApodTableViewController: UITableViewController {
                 updateUI(error: error)
             }
         }
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
+    //MARK: - Functions
+    
     /// Initial updates to the UI.
-    func initialViewUpdate()
+    func fetchingApodViewUpdate()
     {
         imageView.isHidden = true
         titleLabel.text = "Fetching Apod..."
@@ -94,83 +90,41 @@ class ApodTableViewController: UITableViewController {
     {
         imageView.isHidden = true
         titleLabel.text = "Could not fetch Apod...🫤"
-        descriptionTextView.text = "Check the wifi connection.\nIf still not working try again after an hour.\nIf not, try tommorow."
+        descriptionTextView.text = "# Check the wifi connection.\n# Try another date.\nIf still not working try again after an hour.\nIf not, try tommorow."
         likeButton.isEnabled = false
         likeButton.isHidden = true
         copyrightLabel.text = ""
         shareButton.isEnabled = false
         shareButton.isHidden = true
     }
-
-    // MARK: - Table view data source
     
-    /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    // MARK: - Action Functions
+    @IBAction func dateChanged(_ sender: UIDatePicker)
+    {
+        Task {
+            do {
+                fetchingApodViewUpdate()
+                let date = datePicker.date.description.prefix(10)
+                let query = ["api_key":"DEMO_KEY", "date":"\(date)"]
+                let apod = try await apodController.fetchApodInfo(query: query)
+                updateUI(apod: apod)
+            } catch {
+                updateUI(error: error)
+            }
+        }
     }
-     */
     
-    /*
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    @IBAction func likeButtonTapped(_ sender: UIButton)
+    {
+        let alertVC = UIAlertController(title: "Liked!", message: "Apod saved in the liked tabs.", preferredStyle: .actionSheet)
+        let okayAction = UIAlertAction(title: "Okay", style:
+                .default)
+        alertVC.addAction(okayAction)
+        present(alertVC, animated: true)
+        
+        if likeButton.image(for: .normal) == UIImage(systemName: "suit.heart") {
+        } else {
+            likeButton.imageView?.image = UIImage(systemName: "suit.heart")
+        }
     }
-     */
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
